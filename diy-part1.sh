abe3a664c8eeedf80d44e340aa4cc89ab905f625
AletyExcui <77321702+AletyExcui@users.noreply.github.com>
2021-02-14 20:28:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,5 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-git clone https://github.com/fw876/helloworld.git package/openwrt-helloworld
-git clone https://github.com/vernesong/OpenClash.git package/openwrt-OpenClash
+#git clone https://github.com/fw876/helloworld.git package/openwrt-helloworld
+#git clone https://github.com/vernesong/OpenClash.git package/openwrt-OpenClash
